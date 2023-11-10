61e7fcd273beea7f8b62f0cd12ffc41e33a1edc5
Mohammad Akbari <mohammad.akbari@outlook.com>
2023-11-10 16:02:10 +0330

    Consuming a RESTful Web Service

--- src/main/java/com/akbarjimi/restservice/RestServiceApplication.java
+++ src/main/java/com/akbarjimi/restservice/RestServiceApplication.java
@@ -2,13 +2,37 @@ package com.akbarjimi.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RestServiceApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(RestServiceApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    @Profile("!test")
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject("http://localhost:8080/api/random", Quote.class);
+            log.info(quote.toString());
+        };
+    }
 
 }
---
+++ src/main/java/com/akbarjimi/restservice/Value.java
@@ -0,0 +1,7 @@
+package com.akbarjimi.restservice;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Value(long id, String quote) {
+}
