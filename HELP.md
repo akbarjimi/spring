c4bd91c39295cd59697555ce2d0c4ac4529582d0
Mohammad Akbari <mohammad.akbari@outlook.com>
2023-11-27 22:23:58 +0330

    Messaging with Redis

--- HELP.md
+++ HELP.md
@@ -4,16 +4,18 @@
 For further reference, please consider the following sections:
 
 * [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)
-* [Spring Boot Maven Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/3.1.5/maven-plugin/reference/html/)
-* [Create an OCI image](https://docs.spring.io/spring-boot/docs/3.1.5/maven-plugin/reference/html/#build-image)
-* [Spring Web](https://docs.spring.io/spring-boot/docs/3.1.5/reference/htmlsingle/index.html#web)
-* [Thymeleaf](https://docs.spring.io/spring-boot/docs/3.1.5/reference/htmlsingle/index.html#web.servlet.spring-mvc.template-engines)
+* [Spring Boot Maven Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/3.2.0/maven-plugin/reference/html/)
+* [Create an OCI image](https://docs.spring.io/spring-boot/docs/3.2.0/maven-plugin/reference/html/#build-image)
+* [Spring Data Redis (Access+Driver)](https://docs.spring.io/spring-boot/docs/3.2.0/reference/htmlsingle/index.html#data.nosql.redis)
+* [Thymeleaf](https://docs.spring.io/spring-boot/docs/3.2.0/reference/htmlsingle/index.html#web.servlet.spring-mvc.template-engines)
+* [Spring Web](https://docs.spring.io/spring-boot/docs/3.2.0/reference/htmlsingle/index.html#web)
 
 ### Guides
 The following guides illustrate how to use some features concretely:
 
+* [Messaging with Redis](https://spring.io/guides/gs/messaging-redis/)
+* [Handling Form Submission](https://spring.io/guides/gs/handling-form-submission/)
 * [Building a RESTful Web Service](https://spring.io/guides/gs/rest-service/)
 * [Serving Web Content with Spring MVC](https://spring.io/guides/gs/serving-web-content/)
 * [Building REST services with Spring](https://spring.io/guides/tutorials/rest/)
-* [Handling Form Submission](https://spring.io/guides/gs/handling-form-submission/)
 
--- src/main/java/com/akbarjimi/restservice/RestServiceApplication.java
+++ src/main/java/com/akbarjimi/restservice/RestServiceApplication.java
@@ -1,5 +1,6 @@
 package com.akbarjimi.restservice;
 
+import com.akbarjimi.restservice.redis.Receiver;
 import com.akbarjimi.restservice.storage.StorageProperties;
 import com.akbarjimi.restservice.storage.StorageService;
 import org.slf4j.Logger;
@@ -9,10 +10,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.web.client.RestTemplate;
-
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -21,7 +27,19 @@ public class RestServiceApplication {
     private static final Logger log = LoggerFactory.getLogger(RestServiceApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(RestServiceApplication.class, args);
+        ApplicationContext context = SpringApplication.run(RestServiceApplication.class, args);
+        StringRedisTemplate template = context.getBean(StringRedisTemplate.class);
+        Receiver receiver = context.getBean(Receiver.class);
+
+        while (receiver.getCount() == 0) {
+            log.info("Sending messages...");
+            template.convertAndSend("chat", "Hello from redis!");
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @Bean
@@ -40,4 +58,29 @@ public class RestServiceApplication {
         };
     }
 
+    @Bean
+    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+                                            MessageListenerAdapter listenerAdapter) {
+
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
+
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+    @Bean
+    Receiver receiver() {
+        return new Receiver();
+    }
+
+    @Bean
+    StringRedisTemplate template(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
 }
